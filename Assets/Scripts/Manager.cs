61813bb7fa631f566739e949242a6c159a6e09de
julienb <julienb@unity3d.com>
2019-05-08 15:17:03 -0400

    Added Game Demo project
    Game demo project to show how to use Timeline signals.
    Built with Unity 2019.1.0f2

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Manager : MonoBehaviour
+{
+    public PlayableDirector gameDirector;
+    public PlayableDirector deathDirector;
+    public PlayableDirector successTimeline;
+    public Text keyUI;
+
+    static Tuple<KeyCode, string>[] keys =
+    {
+        Tuple.Create(KeyCode.UpArrow, "↑"),
+        Tuple.Create(KeyCode.DownArrow, "↓"),
+        Tuple.Create(KeyCode.LeftArrow, "←"),
+        Tuple.Create(KeyCode.RightArrow, "→")
+    };
+
+    bool isDisplayingKey;
+    KeyCode currentKey;
+
+    public void ShowRandomKey()
+    {
+        if (keyUI == null)
+            return;
+
+        if (!isDisplayingKey)
+        {
+            ShowKeyObject(true);
+            DisplayRandomKey();
+            isDisplayingKey = true;
+        }
+        else //player lost
+        {
+            ShowKeyObject(false);
+            gameDirector.Stop();
+            deathDirector.Play();
+        }
+    }
+
+    public void BeginGame()
+    {
+        if (keyUI != null)
+            ShowKeyObject(false);
+        isDisplayingKey = false;
+    }
+
+    void Update()
+    {
+        if (isDisplayingKey)
+        {
+            if (Input.GetKeyDown(currentKey))
+            {
+                ShowKeyObject(false);
+                isDisplayingKey = false;
+                PlayTimeline(successTimeline);
+            }
+        }
+    }
+
+    static void PlayTimeline(PlayableDirector director)
+    {
+        director.Stop();
+        director.time = 0;
+        director.Play();
+    }
+
+    void ShowKeyObject(bool show)
+    {
+        var parent = keyUI.transform.parent.gameObject;
+        parent.SetActive(show);
+    }
+
+    void DisplayRandomKey()
+    {
+        var keyIndex = Random.Range(0, keys.Length - 1);
+        var (keyCode, keyString) = keys[keyIndex];
+        currentKey = keyCode;
+        keyUI.text = keyString;
+    }
+}
